3f2d2d106342d477f08a57837d09bf2a866ba6d8
bdoyo <127644102+bdoyo@users.noreply.github.com>
2024-12-11 17:19:14 -0600

    Update ApplicationDbContext.cs

--- DNDCompanion/Data/ApplicationDbContext.cs
+++ DNDCompanion/Data/ApplicationDbContext.cs
@@ -10,5 +10,22 @@ namespace DNDCompanion.Data
 
         public DbSet<CharacterModel> Characters { get; set; }
         public DbSet<CampaignModel> Campaigns { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CharacterModel>().HasData(
+                new CharacterModel
+                {
+                    Id = 1,
+                    Name = "Aragorn",
+                    Level = 5,
+                    Class = "Ranger",
+                    Race = "Human",
+                    Background = "Noble",
+                    PlayerId = "1"
+                }
+            );
+        }
     }
 }
